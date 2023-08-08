ba606c52240806ca7cdae06c205a242ff3d33e26
mJawadHaider <jhaider869@gmail.com>
2023-08-08 14:55:14 +0500

    added google analytics in portfolio

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 
 import 'vuetify/styles';
 import vuetify from './plugins/vuetify';
+import VueGtag from 'vue-gtag';
 
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
@@ -15,5 +16,12 @@ const app = createApp(App, {
 
 app.mixin(CommonMixin);
 app.use(vuetify);
+app.use(VueGtag, {
+  appName: 'Jawad Haider',
+  pageTrackerScreenviewEnabled: true,
+  config: { id: 'G-SR7CBSGJGM' },
+});
+
+// G-SR7CBSGJGM
 
 app.mount('#app');
